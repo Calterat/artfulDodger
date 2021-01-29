6fe1e3254c0d4685ef67c304fb8af0a3f9a8b558
Calterat <Calterat@gmail.com>
2021-01-28 22:16:04 -0600

    html is static, so no routes were needed

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const servestatic = require('serve-static');
 const path = require('path');
 const express = require('express');
 
+
+const app = express();
 let PORT = process.env.PORT || 3001
 
-app = express();
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(servestatic(path.join(path.resolve(). '/dist')));
-}
 
-app.listen(PORT, () => {console.log(`API server started on ${app.get('PORT')}`)});
+
+// If you want Vue Hot Reload comment this out AND uncomment the IF statement
+app.use(servestatic(path.join(path.resolve(), 'dist')));
+
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(servestatic(path.join(path.resolve(), 'dist')));
+// }
+
+
+
+
+// starts Express Server
+app.listen(PORT, () => {console.log(`API server started on ${PORT}`)});
