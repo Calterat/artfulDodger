5e7140cc29b09afa5cfcea3eb97b1be147fb47c0
Calterat <Calterat@gmail.com>
2021-01-30 13:39:30 -0600

    added comments on server.js

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ let PORT = process.env.PORT || 3001
 
 
 
-// IF YOU WANT VUE's HOT RELOAD:
+// IF YOU WANT VUE's HOT RELOAD
 // comment out the line below AND run two servers with 'npm run serve' and 'npm start'
 app.use(servestatic(path.join(path.resolve(), 'dist')));
 
+// ALSO
 
+// UNCOMMENT THIS FOR HOT RELOAD
 // LEAVE THIS CODE HERE
 // if(process.env.NODE_ENV === 'production') {
 //   app.use(servestatic(path.join(path.resolve(), 'dist')));
