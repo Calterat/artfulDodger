8a1b434ecc0e71aba1dae1472c0027a6e8c33fbc
Brandon O <b.ondayko@gmail.com>
2021-01-31 12:46:39 -0600

    updated the seeds name

--- db/seed-dodgers.js
+++ db/seed-dodgers.js
@@ -2,12 +2,12 @@ const { Dodger } = require('../models');
 
 const dodgerData = [
   {
-    dodgername: 'testDodger1',
+    username: 'testDodger1',
     email: 'testDodger1@email.com',
     password: 'password1'
   },
   {
-    dodgername: 'testDodger2',
+    username: 'testDodger2',
     email: 'testDodger2@email.com',
     password: 'password2'
   }
