ff011fbb352eb5e8460087502549babf73e59523
Brandon O <b.ondayko@gmail.com>
2021-01-31 12:14:38 -0600

    Landing Page is mostly done. Added both register and login forms. They send out json as email: password: and username: respectivly

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,139 @@
+<template>
+<div>
+    <b-jumbotron header="Welcome To Artful Dodger!" lead="Please Login or Create an Account below" class="mainSplash">
+        <h3>Log In:</h3>
+        <b-container>
+            <b-row align-h="center">
+            <b-form @submit="onSubmitLogIn" @reset="onReset" v-if="show" class="formSizing">
+                <b-form-group
+                    id="input-group-1"
+                    label="Email address:"
+                    label-for="input-1"
+                >
+                    <b-form-input
+                    id="LoginEmail"
+                    v-model="formLogin.email"
+                    type="email"
+                    placeholder="Enter email"
+                    required
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+                    <b-form-input
+                    id="LoginPassword"
+                    v-model="formLogin.password"
+                    type="password"
+                    placeholder="Enter Password"
+                    required
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-button type="submit" variant="primary">Submit</b-button>
+                <b-button type="reset" variant="danger">Reset</b-button>
+            </b-form>
+            </b-row>
+                <b-card class="mt-3" header="Form Data Result">
+                <pre class="m-0">{{ formLogin }}</pre>
+                </b-card>
+        </b-container>
+        <!-- Register Form -->
+        <b-container>
+            <b-row align-h="center">
+            <b-form @submit="onSubmitRegister" @reset="onReset" v-if="show" class="formSizing">
+                <b-form-group
+                    id="input-group-1"
+                    label="Email address:"
+                    label-for="input-1"
+                >
+                    <b-form-input
+                    id="Register"
+                    v-model="formRegister.email"
+                    type="email"
+                    placeholder="Enter email"
+                    required
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                    id="input-group-1"
+                    label="User Name:"
+                    label-for="input-1"
+                >
+                    <b-form-input
+                    id="input-1"
+                    v-model="formRegister.username"
+                    type="input"
+                    placeholder="Enter Username"
+                    required
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+                    <b-form-input
+                    id="input-2"
+                    v-model="formRegister.password"
+                    type="password"
+                    placeholder="Enter Password"
+                    required
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-button type="submit" variant="primary">Submit</b-button>
+                <b-button type="reset" variant="danger">Reset</b-button>
+            </b-form>
+            </b-row>
+                <b-card class="mt-3" header="Form Register">
+                <pre class="m-0">{{ formRegister }}</pre>
+                </b-card>
+        </b-container>
+    </b-jumbotron>
+</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        formLogin: {
+          email: '',
+          password: '',
+        },
+        formRegister: {
+            email: '',
+            username: '',
+            password: '',
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmitLogIn(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.formLogin))
+      },
+      onReset(event) {
+        event.preventDefault()
+        // Reset our form values
+        this.formLogin.email = ''
+        this.formLogin.name = ''
+
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      },
+      onSubmitRegister(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.formRegister))
+        },
+    }
+    }
+</script>
+
+<style>
+    .formSizing {
+        width: 30em;
+    }
+</style>
