6fb4e03cdf2cdfd4d7f66e938be5f8b8c1943780
Calterat <Calterat@gmail.com>
2021-02-01 01:25:00 -0600

    more testing on heroku

--- server.js
+++ server.js
@@ -26,11 +26,12 @@ add additional requirements here
 const app = express();
 let PORT = process.env.PORT || 3001;
 
-app.use(routes);
 
 // IF YOU WANT VUE's HOT RELOAD
 // comment out the line below AND run two servers with 'npm run serve' and 'npm start'
 app.use(servestatic(path.join(path.resolve(), 'dist')));
+app.use(routes);
+
 
 // ALSO
 
