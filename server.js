909bdff44fd31987fc8b31810120b9ef8269923e
Calterat <Calterat@gmail.com>
2021-02-01 01:29:31 -0600

    added sequelize back in for heroku

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ add additional app.use() here
 */
 
 // starts Express Server
-// sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {console.log(`API server lending an ear on port: ${PORT}`)});
-// });
+});
