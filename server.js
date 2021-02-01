90c38fc52814b79d40e7a0886852570e3f846c2e
Calterat <Calterat@gmail.com>
2021-02-01 01:21:13 -0600

    adjusting sequelize to work on heroku

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(servestatic(path.join(path.resolve(), 'dist')));
 //   app.use(servestatic(path.join(path.resolve(), 'dist')));
 // }
 
-app.use(session(sess));
+// app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -51,6 +51,6 @@ add additional app.use() here
 */
 
 // starts Express Server
-sequelize.sync({ force: false }).then(() => {
+// sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {console.log(`API server lending an ear on port: ${PORT}`)});
-});
+// });
